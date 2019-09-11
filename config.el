04b1c425dcd3e6b73dd95575f39d78a9f5b412fa
Jan Dammshäuser <jan.dammshaeuser@7p-group.com>
2019-09-11 21:25:24 +0200

    Add blockfont

--- config.el
+++ config.el
@@ -7,12 +7,13 @@
  minimap-minimum-width 20 ; also slightly smaller minimap
 
  minimap-dedicated-window t ; seems to work better
+ minimap-enlarge-certain-faces nil ; enlarge breaks BlockFont
  )
 
 ;; Disable modeline in minimap buffer
 (add-hook! 'minimap-sb-mode-hook #'hide-mode-line-mode)
 
-;; Change colors of minimap
+;; Change appearance of minimap
 (custom-set-faces
  ;; Change background
  '(minimap-active-region-background
@@ -23,7 +24,11 @@
  '(minimap-current-line-face
    ((((background dark)) (:background "#7F7F7F"))
     (t (:background "#ABABAB")))
-   :group 'minimap))
+   :group 'minimap)
+  '(minimap-font-face
+    ((((background dark)) (:family "BlockFont" :height 30))
+      (t (:family "BlockFont" :height 30)))
+    :group 'minimap))
 
 ;; Enable minimap on startup
 (minimap-mode 1)
